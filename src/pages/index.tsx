35ac8af9bf5dae047b1c8c0b5290f2c21076ddf5
ballsona <ballsona0925@naver.com>
2023-05-25 13:36:44 +0900

    init : 프로젝트 초기 세팅

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+const Home = () => {
+  return (
+    <>
+      <main>
+        <div>
+          <p>Get started by editing</p>
+        </div>
+      </main>
+    </>
+  );
+};
+
+export default Home;
---
+++ src/styles/global.ts
@@ -0,0 +1,25 @@
+import { css } from '@emotion/react';
+
+export const globalStyles = css`
+  @font-face {
+    font-family: 'Pretendard Variable';
+    font-weight: 100 800;
+    font-style: normal;
+    font-display: swap;
+    src: local('Pretendard Variable'),
+      url('/PretendardVariable.woff2') format('woff2-variations');
+  }
+
+  * {
+    box-sizing: border-box;
+    padding: 0;
+    margin: 0;
+    font-family: 'Pretendard Variable';
+  }
+
+  html,
+  body {
+    max-width: 100vw;
+    overflow-x: hidden;
+  }
+`;

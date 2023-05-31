7221e076380811429e68c77c204638ab65780f43
ballsona <ballsona0925@naver.com>
2023-05-31 14:28:16 +0900

    feat : add AnimatePresence for framer-motion

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,13 @@
+import Head from 'next/head';
+import type { AppProps } from 'next/app';
+
 import { globalStyles } from '@/styles/global';
 import { Global } from '@emotion/react';
-import type { AppProps } from 'next/app';
-import Head from 'next/head';
+import { AnimatePresence } from 'framer-motion';
 
 interface ServiceAppProps {}
 
-const MyApp = ({ Component, pageProps }: AppProps<ServiceAppProps>) => {
+const MyApp = ({ Component, pageProps, router }: AppProps<ServiceAppProps>) => {
   return (
     <>
       <Head>
@@ -14,7 +16,9 @@ const MyApp = ({ Component, pageProps }: AppProps<ServiceAppProps>) => {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <Global styles={globalStyles}></Global>
-      <Component {...pageProps} />
+      <AnimatePresence mode="wait">
+        <Component {...pageProps} key={router.route} />
+      </AnimatePresence>
     </>
   );
 };

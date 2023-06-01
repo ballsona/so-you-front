412af62a45b24ade2e3661bbd2b29121d19cefd5
ballsona <ballsona0925@naver.com>
2023-06-01 22:00:55 +0900

    refactor : modify API error handling

--- src/types/api.ts
+++ src/types/api.ts
@@ -1,7 +1,10 @@
 export interface ApiError {
+  /** 에러 코드 */
   code: number;
+  /** 서버에서 전달하는 에러 메세지 */
   errorMessage: string;
-  errorInfo: unknown;
+  /** 에러 관련 정보 (서버에서 받아오는 것 X) */
+  errorInfo?: unknown;
 }
 
 export type ApiResponse<T> = Promise<

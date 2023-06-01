0a22147898dbfcb45d6e9db13550115b2238bfe9
ballsona <ballsona0925@naver.com>
2023-06-01 21:58:18 +0900

    feat : add fields in InfluencerData

--- src/types/influencer.ts
+++ src/types/influencer.ts
@@ -1,10 +1,20 @@
 import { ThemeType } from '@/constants/influencer';
 
 export interface InfluencerDataType {
+  /**  인플루언서 고유 아이디 (계정 아이디 X) */
   influencer_id: number;
+  /** 인플루언서 순위 */
   ranking: number;
+  /** 인플루언서 이름 */
   name: string;
-  subscribeCount: number;
+  /** 소요 기간 (일 단위) */
+  working_time: number;
+  /** 구독자 수 */
+  followersCount: number;
+  /** 관련 카테고리 배열 */
   category: Array<ThemeType>;
+  /** 예상 비용 */
   cost: number;
+  /** 채널 링크 (유투브) */
+  youtube_link?: string;
 }

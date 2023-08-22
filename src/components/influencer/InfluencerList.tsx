2007758c13a18de0ccf9d4c8e82e0c52cdf95d7f
ballsona0925@naver.com <ballsona0925@naver.com>
2023-08-22 15:16:45 +0900

    hotfix: fix style bug in influencer page

--- src/components/influencer/InfluencerList.tsx
+++ src/components/influencer/InfluencerList.tsx
@@ -74,6 +74,7 @@ const Wrapper = styled.div`
   width: 800px;
   background-color: ${COLORS.white};
   box-shadow: 0px 4px 10px -2px rgba(0, 0, 0, 0.1);
+  z-index: 10;
 `;
 
 const Header = styled.div`

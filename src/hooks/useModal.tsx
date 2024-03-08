51b13e3e1868ac40a36b060f14a9562e150971eb
ballsona0925@naver.com <ballsona0925@naver.com>
2024-03-08 14:30:48 +0900

    hotfix: add useCallback to fix infinite re-renders

--- src/hooks/useModal.tsx
+++ src/hooks/useModal.tsx
@@ -1,5 +1,5 @@
 import { modalStateAtom } from '@/stores/modalState';
-import { ReactNode } from 'react';
+import { ReactNode, useCallback } from 'react';
 import { useRecoilState } from 'recoil';
 
 export function useModal() {
@@ -10,22 +10,25 @@ export function useModal() {
    * @param overlay 모달 열려있을 때 배경색 여부(opt)
    * @param name 모달 이름(opt)
    */
-  const openModal = (content: ReactNode, overlay?: boolean, name?: string) => {
-    setModalState({
-      visible: true,
-      content,
-      name,
-      overlay: overlay ?? false,
-    });
-  };
+  const openModal = useCallback(
+    (content: ReactNode, overlay?: boolean, name?: string) => {
+      setModalState({
+        visible: true,
+        content,
+        name,
+        overlay: overlay ?? false,
+      });
+    },
+    [setModalState],
+  );
 
-  const closeModal = () => {
+  const closeModal = useCallback(() => {
     setModalState({
       visible: false,
       content: null,
       name: '',
     });
-  };
+  }, [setModalState]);
 
   return { openModal, closeModal, modalState };
 }

556c36030ad25ab8983bec168e5af457d77ce5f6
ballsona0925@naver.com <ballsona0925@naver.com>
2024-03-08 13:57:31 +0900

    feat: add bundle-analyzer

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
 
-module.exports = {
-  ...nextConfig,
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+
+module.exports = withBundleAnalyzer({
   images: {
     remotePatterns: [
       {
@@ -19,4 +21,4 @@ module.exports = {
     });
     return config;
   },
-};
+});
